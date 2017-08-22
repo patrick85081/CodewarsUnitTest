51c521bde7d3ac68f1192ae5c3c28d03fd3cd7e2
patrick85081 <patrick85081@gmail.com>
2017-08-23 07:40:16 +0800

    新增  奇數排列 題目
    新增  奇數排列 題目

---
+++ CodewarsUnitTest/SortTheOdd/Kata.SortArray.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodewarsUnitTest
+{
+    internal partial class Kata
+    {
+        public static int[] SortArray(int[] array)
+        {
+            return array;
+        }
+    }
+}
---
+++ CodewarsUnitTest/SortTheOdd/SortArrayTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodewarsUnitTest;
+
+namespace CodewarsUnitTest.SortTheOdd
+{
+    [TestClass]
+    public class SortArrayTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(
+                new int[] { }, 
+                Kata.SortArray(new int[] { }));
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.AreEqual(
+                new int[] { 1, 3, 2, 8, 5, 4 },
+                Kata.SortArray(new int[] { 5, 3, 2, 8, 1, 4 }));
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Assert.AreEqual(
+                new int[] { 1, 3, 5, 8, 0 }, 
+                Kata.SortArray(new int[] { 5, 3, 1, 8, 0 }));
+        }
+
+        
+    }
+}
