11b010053e827e0fb3f1288bfcb78c8ac9ba1a01
patrick85081 <patrick85081@gmail.com>
2017-08-23 06:59:49 +0800

    超市排隊 最佳解答
    超市排隊 最佳解答

--- SupermarketQueueTest/Kate.cs
+++ SupermarketQueueTest/Kate.cs
@@ -10,26 +10,12 @@ namespace SupermarketQueueTest
     {
         public static long QueueTime(int[] customers, int number)
         {
-            var itor = customers.GetEnumerator();//as IEnumerator<int>;
-            var clerks = new int[number];
-            
-            var totalTime = 0;
-            var addTime = 0;
-
-            do
+            var regs = Enumerable.Repeat(0, number).ToList();
+            foreach (var cust in customers)
             {
-                for (int i = 0; i < number; i++)
-                {
-                    if (clerks[i] == 0 && itor.MoveNext())
-                        clerks[i] = (int)itor.Current;
-                }
-
-                addTime = clerks.Where(cc => cc > 0).Count() > 0 ? clerks.Where(cc => cc > 0).Min() : 0;
-                totalTime += addTime;
-                clerks = clerks.Select(c => c - addTime).ToArray();
-            } while (addTime > 0);
-
-            return totalTime;
+                regs[regs.IndexOf(regs.Min())] += cust;
+            }
+            return regs.Max();
         }
     }
 }
