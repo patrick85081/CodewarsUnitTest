0215ec0a966bce20ace4bdc8365bd08313f08809
patrick85081 <patrick85081@gmail.com>
2017-08-23 06:55:46 +0800

    超市排隊 通過三個額外案例
    超市排隊 通過三個額外案例

--- SupermarketQueueTest/Kate.cs
+++ SupermarketQueueTest/Kate.cs
@@ -10,9 +10,26 @@ namespace SupermarketQueueTest
     {
         public static long QueueTime(int[] customers, int number)
         {
-            return customers.Count() > number
-                ? customers.Sum() / number
-                : (customers.Count() > 0 ? customers.Max() : 0);
+            var itor = customers.GetEnumerator();//as IEnumerator<int>;
+            var clerks = new int[number];
+            
+            var totalTime = 0;
+            var addTime = 0;
+
+            do
+            {
+                for (int i = 0; i < number; i++)
+                {
+                    if (clerks[i] == 0 && itor.MoveNext())
+                        clerks[i] = (int)itor.Current;
+                }
+
+                addTime = clerks.Where(cc => cc > 0).Count() > 0 ? clerks.Where(cc => cc > 0).Min() : 0;
+                totalTime += addTime;
+                clerks = clerks.Select(c => c - addTime).ToArray();
+            } while (addTime > 0);
+
+            return totalTime;
         }
     }
 }
