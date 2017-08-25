2f65311ca23986b2e801929b9934be00feaf6776
Patrick <patrick85081@gmail.com>
2017-08-25 15:37:39 +0800

    完成平方根 相加
    完成平方根 相加

---
+++ CodewarsUnitTest/SquareIntoSquares/Decompose.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodewarsUnitTest.SquareIntoSquares
+{
+    public partial class Decompose
+    {
+
+        public string decompose(long n)
+        {
+            var list = new List<long>();
+            DecomposeHelper(n * n, n - 1,ref list);
+            list.Reverse();
+            return list.Any() ? string.Join(" ", list) : null;
+        }
+
+        private bool DecomposeHelper(long n, long value, ref List<long> list)
+        {
+            if (n == 0)
+                return true;
+
+            for (var i = value; i > 0; i--)
+            {
+                if (n < 0)
+                    return false;
+                else if (n - (i * i) >= 0)
+                {
+                    var tmp = new List<long>(list);
+
+                    list.Add(i);
+                    var nextN = n - (i * i);
+
+
+                    var result = DecomposeHelper(nextN, i - 1, ref list);
+                    if (result)
+                        return true;
+                    list = tmp;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ CodewarsUnitTest/SquareIntoSquares/DecomposeTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodewarsUnitTest.SquareIntoSquares
+{
+    [TestClass]
+    public class DecomposeTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Decompose d = new Decompose();
+            long n = 11;
+            Assert.AreEqual("1 2 4 10", d.decompose(n));
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Decompose d = new Decompose();
+            long n = 12;
+            Assert.AreEqual("1 2 3 7 9", d.decompose(n));
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Decompose d = new Decompose();
+            long n = 625;
+            Assert.AreEqual("2 5 8 34 624", d.decompose(n));
+        }
+    }
+}
