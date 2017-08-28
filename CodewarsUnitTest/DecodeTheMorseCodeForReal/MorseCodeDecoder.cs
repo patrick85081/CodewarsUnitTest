3ca8ae911bd579ada86fdbb418f74ab925f9f61a
Patrick <patrick85081@gmail.com>
2017-08-28 14:34:15 +0800

    新增 摩斯密碼 真實場景 題目
    新增 摩斯密碼 真實場景 題目

---
+++ CodewarsUnitTest/DecodeTheMorseCodeForReal/MorseCodeDecoder.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodewarsUnitTest.DecodeTheMorseCodeForReal
+{
+    public class Preloaded
+    {
+        public readonly static Dictionary<string, string> MORSE_CODE = new Dictionary<string, string>();
+
+        static Preloaded()
+        {
+            MORSE_CODE.Add(".-", "A");
+            MORSE_CODE.Add("-...", "B");
+            MORSE_CODE.Add("-.-.", "C");
+            MORSE_CODE.Add("-..", "D");
+            MORSE_CODE.Add(".", "E");
+            MORSE_CODE.Add("..-.", "F");
+            MORSE_CODE.Add("--.", "G");
+            MORSE_CODE.Add("....", "H");
+            MORSE_CODE.Add("..", "I");
+            MORSE_CODE.Add(".---", "J");
+            MORSE_CODE.Add("-.-", "K");
+            MORSE_CODE.Add(".-..", "L");
+            MORSE_CODE.Add("--", "M");
+            MORSE_CODE.Add("-.", "N");
+            MORSE_CODE.Add("---", "O");
+            MORSE_CODE.Add(".--.", "P");
+            MORSE_CODE.Add("--.-", "Q");
+            MORSE_CODE.Add(".-.", "R");
+            MORSE_CODE.Add("...", "S");
+            MORSE_CODE.Add("-", "T");
+            MORSE_CODE.Add("..-", "U");
+            MORSE_CODE.Add("...-", "V");
+            MORSE_CODE.Add(".--", "W");
+            MORSE_CODE.Add("-..-", "X");
+            MORSE_CODE.Add("-.--", "Y");
+            MORSE_CODE.Add("--..", "Z");
+            //-----------------------
+            MORSE_CODE.Add(".----", "1");
+            MORSE_CODE.Add("..---", "2");
+            MORSE_CODE.Add("...--", "3");
+            MORSE_CODE.Add("....-", "4");
+            MORSE_CODE.Add(".....", "5");
+            MORSE_CODE.Add("-....", "6");
+            MORSE_CODE.Add("--...", "7");
+            MORSE_CODE.Add("---..", "8");
+            MORSE_CODE.Add("----.", "9");
+            MORSE_CODE.Add("-----", "0");
+            //-----------------------
+            MORSE_CODE.Add(".-.-.-", ".");
+            MORSE_CODE.Add("--..--", ",");
+        }
+
+    }
+
+    // python 解法
+    //https://github.com/joshpeng/Python-Morse-Code/blob/6f999a39b488483f91a77031618fd3460064c7ee/morse/morse.py
+
+    // java 解法
+    //https://github.com/mgruben/morse-code/blob/master/src/MorseCodeDecoder.java
+
+
+    public class MorseCodeDecoder
+    {
+        /**
+         * Given a string in Morse Code, returns the English translation.
+         *
+         * Accept dots, dashes and spaces, returns human-readable message.
+         */
+        public static string decodeBitsAdvanced(string bits)
+        {
+            Console.WriteLine($"Raw Data : {bits}");
+
+            var result = bits.StartEndWith(ch => ch == '1')
+                .DecodeBits()
+                .Replace("111", "-")
+                .Replace("1", ".")
+                .Replace("0000000", "   ")
+                .Replace("000", " ")
+                .Replace("0", "");
+            return result;
+        }
+
+        /**
+         * Given a string of bits, which may or may not begin or end with '0's,
+         * and which may have some variation in the length of the time unit used,
+         * returns the Morse Code translation of this message.
+         *
+         * Accepts 0s and 1s, return dots, dashes and spaces
+         *
+         */
+        public static string decodeMorse(string morseCode)
+        {
+            // Map morse code using map Preloaded.MORSE_CODE
+            Console.WriteLine($"Input Morse : {morseCode}");
+
+            var words = morseCode.Split(new string[] { "   " }, StringSplitOptions.None)
+                .Select(word => word.Split(new char[] { ' ' }, StringSplitOptions.None))
+                .Select(word => word.Select(morse => Preloaded.MORSE_CODE.ContainsKey(morse) ? Preloaded.MORSE_CODE[morse] : "").ToArray())
+                .Select(word => string.Join("", word))
+                .Where(word => word != string.Empty)
+                .ToArray();
+
+            var result = string.Join(" ", words);
+            Console.WriteLine($"Result : {result}");
+            return result;
+        }
+    }
+
+
+    static class Helper
+    {
+
+        public static string StartEndWith(this string source, Predicate<char> select)
+        {
+            return source.FirstStartWith(select).LastEndtWith(select).CoverterToString();
+        }
+
+        public static string DecodeBits(this string source)
+        {
+            var rate = source.GetTranslateRate();
+
+            return source.Replace(Enumerable.Repeat("1", rate).CoverterToString(), "1")
+                .Replace(Enumerable.Repeat("0", rate).CoverterToString(), "0");
+        }
+
+        private static string CoverterToString(this IEnumerable<string> source)
+        {
+
+            return string.Join("", source.ToArray());
+        }
+
+        private static string CoverterToString(this IEnumerable<char> source)
+        {
+            return new string(source.ToArray());
+        }
+
+        private static IEnumerable<char> FirstStartWith(this IEnumerable<char> source, Predicate<char> select)
+        {
+            bool isStart = false;
+            foreach (var ch in source)
+            {
+                if (!isStart)
+                    isStart = select(ch);
+
+                if (isStart)
+                    yield return ch;
+            }
+        }
+
+        private static IEnumerable<char> LastEndtWith(this IEnumerable<char> source, Predicate<char> select)
+        {
+            return source.Reverse()
+                .FirstStartWith(select)
+                .Reverse();
+        }
+
+        private static int GetTranslateRate(this string rawData)
+        {
+            var hCollect = rawData.Split(new char[] { '0' }, StringSplitOptions.RemoveEmptyEntries);
+            var Hight = hCollect.Length > 0 ? hCollect.Min(single => single.Length) : 999;
+
+            var lCollect = rawData.Split(new char[] { '1' }, StringSplitOptions.RemoveEmptyEntries);
+            var Low = lCollect.Length > 0 ? lCollect.Min(single => single.Length) : 999;
+
+            var rate = Math.Min(Hight, Low);
+            Console.WriteLine($"速率 : {rate}");
+            return rate;
+        }
+    }
+}
---
+++ CodewarsUnitTest/DecodeTheMorseCodeForReal/Readme.md
@@ -0,0 +1,122 @@
+﻿# Decode the Morse code, advanced
+[題目來源 : Codewars](http://www.codewars.com/kata/decode-the-morse-code-for-real)
+
+### 題目簡介
+
+在這個題目裡你必須處理“現實生活”情景，莫爾斯電碼的傳輸速度在整個消息中略有不同，
+因為它是由一個非完美的人類發送的。此外，採樣頻率可能不是典型“點”長度的倍數。
+
+例如，可能實際上可以收到以下消息 ```···· · −·−−   ·−−− ··− −·· ·``` ，即是HEY JUDE。
+
+```
+0000000011011010011100000110000001111110100111110011111100000000000111011111111011111011111000000101100011111100000111110011101100000100000
+```
+
+正如你所看到的，這種傳輸通常是根據標準準確的，但是一些點(.)和破折號(-)和暫停比其他的短或稍長一些。
+
+還要注意的是，與先前的題目相比，估計的平均速率（每點的位數）可能不是整數，
+因為發送電報的是人類，並且不知道接收端採樣頻率。
+
+例如，您可以每秒採樣10次 (每次採樣 100ms)，而操作員發送的 點 和 短停頓 為110-170ms長。
+每秒取樣10次是足夠的速度 (意思是每個點和暫停都反映在輸出中，沒有任何東西被忽略)，
+點將反映為1或 11，但如果你試圖估計速率 (bit / 一點)，它不會是1或 2，它將是大約（110 + 170）/ 2 / 100 = 1.4。
+您的演算法應該處理這樣的情況。
+
+另外請記住，每的消息可能被不同的運營商發送，所以它的速率和其他特性將是不同的。
+所以你必須獨立地分析每個訊息（即測試），而不依賴於之前的訊息。另一方面，我們假設傳輸特性會在同一則訊息中保持一致，
+因此您必須分析整個消息使得解碼正確。
+所謂的一致性意表示如果在訊息中“11111”的開頭是一個點(.)，那“111111”是短劃線(-)，那麼在同一則訊息中的任何地方也是如此。
+同理“00000”絕對是一個短暫（字符）的暫停，“000000”是一個長（字符間）的暫停。
+
+### 你的任務是實現兩個功能：
+
+1. ```decodeBitsAdvanced(string bits)```方法，應該找到消息的傳輸速率的估計，
+   注意消息中可能發生的輕微的速度變化，將消息正確解碼為點，破折號和空格（字符之間的一個，三個字之間），
+   並將其作為字符串返回。請注意，一些額外的0可能會自然地發生在消息的開頭和結尾，確保忽略它們。
+   如果消息為空或僅包含0，則返回空字符串。此外，如果您遇到困難，如果1的特定順序是點或破折號，
+   假設它是一個點。如果卡住了，請查看這個想法。
+
+2. ```decodeMorse（string morseCode）```方法，它將使用前一個函數的輸出，並返回一個人類可讀的英文字符串。
+   如果輸入為空字元或空白字元，則返回空字元。
+
+
+對於c#，摩爾斯碼表預先加載為```Preloaded.MORSE_CODE```的```Dictionary<string, string>```。
+
+
+```c#
+public static class Preloaded
+{
+    public static Dictionary<string, string> MORSE_CODE { get; }
+}
+```
+
+當然，並不是所有的消息都可能被完全自動解碼。但是您可能會確定所有的測試字符串都將有效，
+以至於可以如上所述可靠地解碼，因此您可以跳過檢查錯誤和異常情況，盡可能地確定消息是什麼！
+
+祝你好運！！
+
+
+## 原文
+
+In this kata you have to deal with "real-life" scenarios, 
+when Morse code transmission speed slightly varies throughout the message as it is sent by a non-perfect human operator. 
+Also the sampling frequency may not be a multiple of the length of a typical "dot".
+For example, the message ```HEY JUDE```, 
+that is ```···· · −·−−   ·−−− ··− −·· ·``` may actually be received as follows:
+```
+0000000011011010011100000110000001111110100111110011111100000000000111011111111011111011111000000101100011111100000111110011101100000100000
+```
+As you may see, this transmission is generally accurate according to the standard, 
+but some dots and dashes and pauses are a bit shorter or a bit longer than the others.
+
+Note also, that, in contrast to the previous kata, 
+the estimated average rate (bits per dot) may not be a whole number – as the hypotetical transmitter is a human and doesn't know anything about the receiving side sampling rate.
+
+For example, you may sample line 10 times per second (100ms per sample), 
+while the operator transmits so that his dots and short pauses are 110-170ms long. 
+Clearly 10 samples per second is enough resolution for this speed 
+(meaning, each dot and pause is reflected in the output, nothing is missed), 
+and dots would be reflected as 1 or 11, but if you try to estimate rate (bits per dot), 
+it would not be 1 or 2, it would be about (110 + 170) / 2 / 100 = 1.4. 
+Your algorithm should deal with situations like this well.
+
+Also, remember that each separate message is supposed to be possibly sent by a different operator, 
+so its rate and other characteristics would be different. 
+So you have to analyze each message (i. e. test) independently, 
+without relying on previous messages. 
+On the other hand, we assume the transmission charactestics remain consistent throghout the message, 
+so you have to analyze the message as a whole to make decoding right. 
+Consistency means that if in the beginning of a message '11111' is a dot and '111111' is a dash, 
+then the same is true everywhere in that message. Moreover, 
+it also means '00000' is definitely a short (in-character) pause, 
+and '000000' is a long (between-characters) pause.
+
+### That said, your task is to implement two functions:
+
+1. Function ```decodeBitsAdvanced(bits)```, 
+   that should find an estimate for the transmission rate of the message, 
+   take care about slight speed variations that may occur in the message, 
+   correctly decode the message to dots ```.```, 
+   dashes ```-``` and spaces (one between characters, three between words) and return those as a string. 
+   Note that some extra ```0```'s may naturally occur at the beginning and the end of a message, 
+   make sure to ignore them. If message is empty or only contains ```0```'s, return empty string. 
+   Also if you have trouble discerning if the particular sequence of ```1```'s is a dot or a dash, 
+   assume it's a dot. If stuck, check this for ideas.
+
+2. Function ```decodeMorse(morseCode)```, 
+   that would take the output of the previous function and return a human-readable string. 
+   If the input is empty string or only contains spaces, return empty string.
+
+The Morse code table is preloaded for you as ```MORSE_CODE``` dictionary, feel free to use it. 
+(For C, the function 'morse_code' acts like the dictionary. For C++ a map is used.)
+
+``` c++
+const char *morse_code (const char *dotsdashes);
+```
+
+Of course, not all messages may be fully automatically decoded. 
+But you may be sure that all the test strings would be valid to the point that they could be reliably decoded as described above, 
+so you may skip checking for errors and exceptions, 
+just do your best in figuring out what the message is!
+
+Good luck!
