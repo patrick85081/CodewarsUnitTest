0eda6410e1e8aa8e8d542461c6f34825992c21fc
Patrick <patrick85081@gmail.com>
2017-09-13 11:56:32 +0800

    新增 IsInterestingTests 題目

---
+++ CodewarsUnitTest/CatchingCarMileageNumbers/IsInterestingTests.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodewarsUnitTest.CatchingCarMileageNumbers
+{
+    [TestClass]
+    public class IsInterestingTests
+    {
+        [TestMethod]
+        public void BoaringNumberTest()
+        {
+            Assert.AreEqual(0, Kata.IsInteresting(3, new List<int>() {1337, 256}));
+            Assert.AreEqual(0, Kata.IsInteresting(3236, new List<int>() { 1337, 256 }));
+        }
+
+        [TestMethod]
+        public void NoNearingAweosePhrasesTest()
+        {
+            Assert.AreEqual(0, Kata.IsInteresting(11207, new List<int>() { }));
+            Assert.AreEqual(0, Kata.IsInteresting(11208, new List<int>() { }));
+            Assert.AreEqual(1, Kata.IsInteresting(11209, new List<int>() { }));
+            Assert.AreEqual(1, Kata.IsInteresting(11210, new List<int>() { }));
+            Assert.AreEqual(2, Kata.IsInteresting(11211, new List<int>() { }));
+        }
+
+        [TestMethod]
+        public void NearingAwesomePhraseTest()
+        {
+            Assert.AreEqual(1, Kata.IsInteresting(1335, new List<int>() { 1337, 256 }));
+            Assert.AreEqual(1, Kata.IsInteresting(1336, new List<int>() { 1337, 256 }));
+            Assert.AreEqual(2, Kata.IsInteresting(1337, new List<int>() { 1337, 256 }));
+        }
+
+        [TestMethod]
+        public void ShouldWorkTest()
+        {
+            Assert.AreEqual(0, Kata.IsInteresting(3, new List<int>() { 1337, 256 }));
+            Assert.AreEqual(1, Kata.IsInteresting(1336, new List<int>() { 1337, 256 }));
+            Assert.AreEqual(2, Kata.IsInteresting(1337, new List<int>() { 1337, 256 }));
+            Assert.AreEqual(0, Kata.IsInteresting(11208, new List<int>() { 1337, 256 }));
+            Assert.AreEqual(1, Kata.IsInteresting(11209, new List<int>() { 1337, 256 }));
+            Assert.AreEqual(2, Kata.IsInteresting(11211, new List<int>() { 1337, 256 }));
+        }
+
+        [TestMethod]
+        public void SequentialTest()
+        {
+            Assert.AreEqual(1, Kata.IsInteresting(1233, new List<int>() { 1337, 256 }));
+            Assert.AreEqual(2, Kata.IsInteresting(1234, new List<int>() { 1337, 256 }));
+            Assert.AreEqual(0, Kata.IsInteresting(1235, new List<int>() { 1337, 256 }));
+
+            Assert.AreEqual(1, Kata.IsInteresting(67889, new List<int>() { 1337, 256 }));
+            Assert.AreEqual(2, Kata.IsInteresting(67890, new List<int>() { 1337, 256 }));
+            Assert.AreEqual(0, Kata.IsInteresting(67891, new List<int>() { 1337, 256 }));
+
+            Assert.AreEqual(1, Kata.IsInteresting(4320, new List<int>() { 1337, 256 }));
+            Assert.AreEqual(2, Kata.IsInteresting(4321, new List<int>() { 1337, 256 }));
+            Assert.AreEqual(0, Kata.IsInteresting(4322, new List<int>() { 1337, 256 }));
+        }
+
+        [TestMethod]
+        public void AdvanceTest()
+        {
+            Assert.AreEqual(1, Kata.IsInteresting(109, new List<int>() { 1337, 256 }));
+        }
+    }
+}
---
+++ CodewarsUnitTest/CatchingCarMileageNumbers/Kata.IsInteresting.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodewarsUnitTest.CatchingCarMileageNumbers
+{
+    public static partial class Kata
+    {
+        public static int IsInteresting(int number, List<int> awesomePhrases)
+        {
+            var nearNumber = Enumerable.Range(number + 1, 2)
+                .Where(n => n != number);
+            
+
+            var result = IsInterestingImpl(number, awesomePhrases) ? 2 : 
+                (nearNumber.Any(n => IsInterestingImpl(n, awesomePhrases)) ? 1 : 0);
+
+            Console.WriteLine($"{number}, {string.Join(", ", awesomePhrases.Select(n => n.ToString()).ToArray())} => {result}");
+
+            return result;
+        }
+
+        private static bool IsInterestingImpl(int number, IEnumerable<int> awesomePhrases)
+        {
+            return awesomePhrases.Contains(number) || IsSpecialNumber(number);
+        }
+
+        private static bool IsSpecialNumber(int number)
+        {
+            var word = number.ToString();
+
+            if (word.Length <= 2)
+                return false;
+
+            return IsAllZero(word) || IsNumberSame(word) || IsPalindrome(word) || IsSequential(word);
+        }
+
+        private static bool IsSequential(string word)
+        {
+            var sequentail = "01234567890";
+            var sequentailReverse = "09876543210";
+
+            return sequentail.Contains(word) || sequentailReverse.Contains(word);
+        }
+
+        private static bool IsPalindrome(string word)
+        {
+            var v1 = word.Take(word.Length / 2);
+            var v2 = word.Reverse().Take(word.Length / 2);
+            var isPalindrome = v1.EnumerableIsSame(v2);
+            return isPalindrome;
+        }
+
+        private static bool IsNumberSame(string word)
+        {
+            return word.GroupBy(w => w).Count() == 1;
+        }
+
+        private static bool IsAllZero(string word)
+        {
+            return word.Skip(1).All(w => w == '0');
+        }
+
+
+        private static bool EnumerableIsSame<T>(this IEnumerable<T> source, IEnumerable<T> compare)
+        {
+            return (source.Count() == compare.Count()) &&
+                   source.Zip(compare, (s1, s2) => s1.Equals(s2)).All(result => result);
+        }
+    }
+}
